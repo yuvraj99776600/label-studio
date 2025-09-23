14c121479e8da63e75c9a367d4714dac094ea855
Brandon Martel <brandonmartel@gmail.com>
2025-09-23 11:41:28 -0500

    the imports of CurrentContext have to be lazy

--- label_studio/core/current_request.py
+++ label_studio/core/current_request.py
@@ -1,8 +1,9 @@
-from core.current_context import CurrentContext
 from django.middleware.common import CommonMiddleware
 
 
 def get_current_request():
+    from core.current_context import CurrentContext
+
     """returns the request object for this thread"""
     result = CurrentContext.get_request()
     return result
@@ -10,13 +11,19 @@ def get_current_request():
 
 class ThreadLocalMiddleware(CommonMiddleware):
     def process_request(self, request):
+        from core.current_context import CurrentContext
+
         CurrentContext.set_request(request)
 
     def process_response(self, request, response):
+        from core.current_context import CurrentContext
+
         if CurrentContext.get_request():
             CurrentContext.clear()
         return response
 
     def process_exception(self, request, exception):
+        from core.current_context import CurrentContext
+
         if CurrentContext.get_request():
             CurrentContext.clear()
