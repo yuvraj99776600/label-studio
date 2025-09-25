db45c0ed0bae46231a2698e32f6e7cfcf5f416e7
Wesley Lima <wesley@humansignal.com>
2025-09-25 18:34:38 -0400

    Mocked up CurrentContext

--- label_studio/core/current_request.py
+++ label_studio/core/current_request.py
@@ -1,106 +1,56 @@
-import threading
-from typing import TYPE_CHECKING, Any, Optional
+from threading import local
 
 from django.core.signals import request_finished
 from django.dispatch import receiver
 from django.middleware.common import CommonMiddleware
 
-if TYPE_CHECKING:
-    from django.http import Request
-    from users.models import User
-
-
-_thread_locals = threading.local()
-
-
-class CurrentContext:
-    """
-    Context object available across the entire request or worker context (not shared between requests or workers, i.e. each request or worker has its own context)
-    This is used to store contextual information that is needed across the entire request/worker context like organization_id, user, etc.
-
-    In workers this needs to explicitly be set when running the worker.
-    In requests this is set automatically by the ThreadLocalMiddleware.
-    """
-
-    @classmethod
-    def set(cls, key: str, value: Any, shared: bool = True) -> None:
-        if not hasattr(_thread_locals, 'data'):
-            _thread_locals.data = {}
-        if not hasattr(_thread_locals, 'job_data'):
-            _thread_locals.job_data = {}
-
-        if shared:
-            _thread_locals.job_data[key] = value
-        else:
-            _thread_locals.data[key] = value
-
-    @classmethod
-    def get(cls, key: str, default: Any = None) -> Any:
-        return getattr(_thread_locals, 'job_data', {}).get(key, getattr(_thread_locals, 'data', {}).get(key, default))
-
-    @classmethod
-    def get_request(cls) -> Optional['Request']:
-        return getattr(_thread_locals, 'request', None)
-
-    @classmethod
-    def set_request(cls, request: 'Request') -> None:
-        _thread_locals.request = request
-        if request.user:
-            cls.set_user(request.user)
-
-    @classmethod
-    def get_organization_id(cls) -> Optional[int]:
-        return cls.get('organization_id')
-
-    @classmethod
-    def set_organization_id(cls, organization_id: int) -> None:
-        cls.set('organization_id', organization_id)
-
-    @classmethod
-    def get_user(cls) -> Optional['User']:
-        return cls.get('user')
-
-    @classmethod
-    def set_user(cls, user: 'User') -> None:
-        cls.set('user', user)
-        if getattr(user, 'active_organization_id', None):
-            cls.set_organization_id(user.active_organization_id)
-
-    @classmethod
-    def is_job(cls) -> bool:
-        return cls.get_request() is None
-
-    @classmethod
-    def get_job_data(cls) -> dict:
-        """
-        This data will be shared to jobs spawned by the current thread.
-        """
-        return getattr(_thread_locals, 'job_data', {})
-
-    @classmethod
-    def clear(cls) -> None:
-        if hasattr(_thread_locals, 'data'):
-            delattr(_thread_locals, 'data')
-
-        if hasattr(_thread_locals, 'job_data'):
-            delattr(_thread_locals, 'job_data')
-
-        if hasattr(_thread_locals, 'request'):
-            delattr(_thread_locals, 'request')
+_thread_locals = local()
 
 
 def get_current_request():
-
     """returns the request object for this thread"""
-    result = CurrentContext.get_request()
+    result = getattr(_thread_locals, 'request', None)
     return result
 
 
 class ThreadLocalMiddleware(CommonMiddleware):
     def process_request(self, request):
-        CurrentContext.set_request(request)
+        _thread_locals.request = request
 
 
 @receiver(request_finished)
 def clean_request(sender, **kwargs):
-    CurrentContext.clear()
+    if hasattr(_thread_locals, 'request'):
+        del _thread_locals.request
+
+
+class CurrentContext:
+    @classmethod
+    def get_user(cls):
+        return _thread_locals.request.user
+    
+    @classmethod
+    def set_user(self, user):
+        _thread_locals.request.user = user
+    
+    @classmethod
+    def clear(self):
+        del _thread_locals.request
+
+    @classmethod
+    def get_organization_id(self):
+        return _thread_locals.request.user.active_organization.id
+    
+    @classmethod
+    def set_organization_id(self, organization_id):
+        _thread_locals.request.user.active_organization_id = organization_id
+        _thread_locals.request.user.save()
+    
+    @classmethod
+    def clear(self):
+        del _thread_locals.request
+
+
+    @classmethod
+    def get_job_data(self):
+        return {}
