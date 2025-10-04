0542cd445990a03bf056ff55b993f9e4d7ccf3fb
makseq <makseq@gmail.com>
2025-10-04 01:34:39 +0100

    fix: Allow internal HumanSignal Org use plugins

--- label_studio/core/feature_flags/utils.py
+++ label_studio/core/feature_flags/utils.py
@@ -5,7 +5,10 @@ def get_user_repr(user):
     user_data = {'email': user.email}
     user_data['key'] = user_data['email']
     if user.active_organization is not None:
-        user_data['custom'] = {'organization': user.active_organization.created_by.email}
+        user_data['custom'] = {
+            'organization': user.active_organization.created_by.email,
+            'organization_id': user.active_organization.id,
+        }
     else:
-        user_data['custom'] = {'organization': None}
+        user_data['custom'] = {'organization': None, 'organization_id': None}
     return user_data
