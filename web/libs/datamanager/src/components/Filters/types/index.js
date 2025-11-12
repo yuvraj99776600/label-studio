d684b89c2c1e593d425854afb39ef8be3ce851aa
Yousif Yassi <yousif@heartex.com>
2025-11-12 15:31:08 -0500

    feat: FIT-976: [FSM UI] Data Manager State Column

--- web/libs/datamanager/src/components/Filters/types/index.js
+++ web/libs/datamanager/src/components/Filters/types/index.js
@@ -4,4 +4,4 @@ export { DateFilter as Date } from "./Date";
 export { DatetimeFilter as Datetime } from "./Datetime";
 export { ListFilter as List } from "./List";
 export { NumberFilter as Number, NumberFilter as AgreementSelected } from "./Number";
-export { StringFilter as Image, StringFilter as String } from "./String";
+export { StringFilter as Image, StringFilter as String, StringFilter as TaskState } from "./String";
