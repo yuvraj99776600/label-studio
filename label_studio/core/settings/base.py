a78e1ed3931aa1e7129d42c060ff94216b8003b0
Matt Bernstein <matt@humansignal.com>
2025-11-26 13:08:54 -0500

    fix setting name

--- label_studio/core/settings/base.py
+++ label_studio/core/settings/base.py
@@ -405,7 +405,7 @@ SPECTACULAR_SETTINGS = {
     ],
     'CONTACT': {'url': 'https://labelstud.io'},
     'X_LOGO': {'url': '../../static/icons/logo-black.svg'},
-    'ENUM_ADD_EXPLICIT_BLANK_NULL_CHOICES': False,
+    'ENUM_ADD_EXPLICIT_BLANK_NULL_CHOICE': False,
 }
 
 SENTRY_DSN = get_env('SENTRY_DSN', None)
