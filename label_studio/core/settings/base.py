7943bbbe46c54feca8458df119c40248b7ccb3c4
Matt Bernstein <matt@humansignal.com>
2025-11-26 11:39:01 -0500

    fix: FIT-1049: unblock Fern generation

--- label_studio/core/settings/base.py
+++ label_studio/core/settings/base.py
@@ -405,6 +405,7 @@ SPECTACULAR_SETTINGS = {
     ],
     'CONTACT': {'url': 'https://labelstud.io'},
     'X_LOGO': {'url': '../../static/icons/logo-black.svg'},
+    'ENUM_ADD_EXPLICIT_BLANK_NULL_CHOICES': False,
 }
 
 SENTRY_DSN = get_env('SENTRY_DSN', None)
